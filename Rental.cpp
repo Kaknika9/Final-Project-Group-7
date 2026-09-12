a9e8237efbe9e251c8ae14bcf4d25bfa4d4e640e
Kaknika9 <kaknika189999@icloud.com>
2026-09-12 14:09:19 +0700

    feat: add default constructor

--- Rental.cpp
+++ Rental.cpp
@@ -3,3 +3,13 @@
 
 using namespace std;
 
+//default constructor
+Rental::Rental(){
+    rentalId = 0;
+    customerId = 0;
+    vehicleId = 0;
+    rentalDays = 0;
+    dailyRate = 0.0;
+    totalCost = 0.0;
+    active = false;
+}
